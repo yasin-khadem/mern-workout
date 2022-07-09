1d5f769000e7ba7970580c5c0b98604dd509cb0b
yasin-khadem <yasin.khadem77@gmail.com>
2022-07-09 15:49:44 +0430

    setup create api for workout

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../models/workoutModel");
 
 router.get("/", (req, res) => {
-    res.json({msg:"get all workouts"});
-})
+  res.json({ msg: "get all workouts" });
+});
 
 router.get("/:id", (req, res) => {
-    res.json({msg:"get workout by id"});
-})
+  res.json({ msg: "get workout by id" });
+});
 
-router.post("/", (req, res) => {
-    res.json({msg:"create a new workout"});
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(201).json(workout);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 });
 
 router.delete("/:id", (req, res) => {
-    res.json({msg:"delete a workout"});
+  res.json({ msg: "delete a workout" });
 });
 
 router.delete("/:id", (req, res) => {
-    res.json({msg:"delete a workout"});
+  res.json({ msg: "delete a workout" });
 });
 
 router.patch("/:id", (req, res) => {
-    res.json({msg:"update a workout"});
+  res.json({ msg: "update a workout" });
 });
 
 module.exports = router;
