8f60ee66c8033e3a54c09e640516c5314a761e1f
yasin-khadem <yasin.khadem77@gmail.com>
2022-07-09 15:04:01 +0430

    install and setup mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 
 // middleware
 app.use((req, res, next) => {
-    console.log(req.path, req.method, req.url);
-    next();
+  console.log(req.path, req.method, req.url);
+  next();
 });
 
 // routes
-app.use('/api/workouts',workoutRoutes)
+app.use("/api/workouts", workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 4000");
-});
+//connect to the database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port 4000");
+    });
+  })
+  .catch((err) => console.error(err));
