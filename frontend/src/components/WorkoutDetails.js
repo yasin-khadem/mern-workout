8680dd3e94bed1c76c10884ec9c2e879676e069e
yasin-khadem <yasin.khadem77@gmail.com>
2022-07-18 02:33:59 +0430

    install date-fns and insert icon for delete btn

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteWorkout } from "../slices/workoutSlice";
+import { formatDistanceToNow } from "date-fns";
 
 export default ({ workout }) => {
   const dispatch = useDispatch();
@@ -28,8 +29,10 @@ export default ({ workout }) => {
         <strong>Number of reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
-      <span onClick={handleDelete}>delete</span>
+      <p>{formatDistanceToNow(new Date(workout.createdAt),{addSuffix:true})}</p>
+      {/* <span onClick={handleDelete}>delete</span> */}
+      <span className="material-symbols-outlined" onClick={handleDelete}>delete</span>
+
     </div>
   );
 };
