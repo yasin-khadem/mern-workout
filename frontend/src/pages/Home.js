ec60eea1fa0e95973baeb6687c2fa38a73a42382
yasin-khadem <yasin.khadem77@gmail.com>
2022-07-17 22:23:23 +0430

    add proxy for backend , add workoutDetails component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+
 function Home() {
+  const [workouts, setWorkouts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api/workouts");
+      const body = await response.json();
+      if (response.ok) {
+        setWorkouts(body);
+      }
+    };
+    
+    fetchData();
+  }, []);
+  console.log(workouts)
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
     </div>
   );
-};
+}
 export default Home;
